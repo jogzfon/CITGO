1f9e45db88de9fb70092bc4822e019a631f03fd6
jogzfon <jogzfon@gmail.com>
2023-10-03 23:36:08 +0800

    FeedBack

--- CITGO/event_evaluation_submission/models.py
+++ CITGO/event_evaluation_submission/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from CITGO.teacher_assignment.models import Teacher, Event
 
 # Create your models here.
+class FeedBack(models.Model):
+    feedbackID = models.IntegerField(null=False, primary_key=True)
+    feedback = models.CharField(max_length=500, null=False)
+    studentID = models.ManyToManyRel()
+    teacherID = models.ManyToManyRel(Teacher)
+    eventID = models.ForeignKey(Event)
