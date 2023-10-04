7d889db196ef9a730f9ac7873b43fe219274ba33
jogzfon <jogzfon@gmail.com>
2023-10-04 13:22:04 +0800

    sheesh changes

--- CITGO/EventAttendanceTracker/models.py
+++ CITGO/EventAttendanceTracker/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class AttendanceTracker(models.Model):
-    trackingId = models.IntegerField(max_length=15, null=False,primary_key = True)
+    trackingId = models.IntegerField(null=False,primary_key = True)
     students_attending = models.CharField(max_length = 256)
    # studentID = models.ForeignKey()
    # ticketID = models.ForeignKey()
--- CITGO/student_registration/models.py
+++ CITGO/student_registration/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 
 class Student(models.Model):
-    StudentID = models.IntegerField(max_length=15, null=False, primary_key=True)
+    StudentID = models.IntegerField(null=False, primary_key=True)
     StudentName = models.CharField(max_length=255, null=False)
-    Year = models.IntegerField(max_length=10, null=False)
+    Year = models.IntegerField(null=False)
     Course = models.CharField(max_length=255, null=False)
 
     def __str__(self):
